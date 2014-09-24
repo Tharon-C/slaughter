84766c6243d223548deed1d63eda1669995510c6
Kellan Cummings <kellan@taglinegroup.com>
2014-09-24 10:31:59 -0600

    small css tweek selecting.social instead of img for top side wdgts

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 			</div>    
           <p class="copyright">
 					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
-                All rights reserved. <a href="http://taglinegroup.com"> Website Design</a> by Tagline.
+                All rights reserved. <a href="http://taglinegroup.com"> Website Design</a> by Tagline.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1102 W Grant Rd. Tucson, AZ 85705
 				</p>  
 			</footer>	
 		</div>
